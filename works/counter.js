6a191acb614fa0756d2dbf4e45a95194e990b77c
toyamanya <ryotoyamaiii@gmail.com>
2019-05-11 02:03:34 +0900

    add counter site write up 3 files

---
+++ works/counter.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+const countUp = () => {
+  count++;
+  const countElement = document.querySelector("#count");
+  countElement.innerText = "回数:" + count;
+};
+
+const countDel = () => {
+  const countElement = document.querySelector("#count");
+  countElement.innerText = "回数:" + 0;
+};
